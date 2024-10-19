c05de130e3ca8c4d1923f0f94336e01282ad1d18
bumipl <szala2002.pl@gmail.com>
2024-10-19 22:40:34 +0200

    second push of my app

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("welcomeBtn").addEventListener("click", function() {
-    const welcomeMessage = document.getElementById("welcomeMessage");
-    welcomeMessage.textContent = "Hello, DevOps Engineer! Java Script is now live.";
-    welcomeMessage.classList.remove("hidden");
-});
